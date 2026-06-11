6d3cdea1e60fa22061e22840dc5f448098d22659
mrayushcodes <noobtoprogameronline2@gmail.com>
2026-06-11 20:19:38 +0530

    Update map-view.tsx

--- components/map-view.tsx
+++ components/map-view.tsx
@@ -81,7 +81,7 @@ const MapView = () => {
       const L = (await import('leaflet')).default;
       
       // Fix for default markers in Next.js
-      delete L.Icon.Default.prototype._getIconUrl;
+      delete (L.Icon.Default.prototype as any)._getIconUrl;
       L.Icon.Default.mergeOptions({
         iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
         iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
